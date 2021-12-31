3d3604b1026749a6aaf1b304efd3384c7711a704
Dániel Sebestyén <dannystyleart@gmail.com>
2021-12-31 15:36:21 +0100

    Add more border connections data

--- src/dataset/landborders.ts
+++ src/dataset/landborders.ts
@@ -1,122 +1,122 @@
 export default {
   AFG: ['UZB', 'TJK', 'PAK', 'IRN', 'TKM'],
-  ALA: [],
+  ALA: [], // Island
   ALB: ['MNE', 'XXK', 'MKD', 'GRC'],
   DZA: ['TUN', 'LBY', 'NER', 'MLI', 'MRT', 'ESH', 'MAR'],
-  ASM: [],
+  ASM: [], // Island
   AND: ['FRA', 'ESP'],
   AGO: ['COD', 'ZMB', 'BWA', 'NAM'],
-  AIA: [],
-  ATA: [],
-  ATG: [],
+  AIA: [], // Island
+  ATA: [], // Island
+  ATG: [], // Island
   ARG: ['BOL', 'PRY', 'BRA', 'URY', 'CHL'],
   ARM: ['GEO', 'AZE', 'IRN', 'TUR'],
-  ABW: [],
-  AUS: [],
+  ABW: [], // Island
+  AUS: [], // Island
   AUT: ['CZE', 'SVK', 'HUN', 'SVN', 'ITA', 'LIE', 'CHE', 'DEU'],
   AZE: ['RUS', 'IRN', 'ARM', 'GEO'],
-  BHS: [],
+  BHS: [], // Island
   BHR: ['SAU', 'QAT'],
   BGD: ['IND'],
-  BRB: [],
+  BRB: [], // Island
   BLR: ['LVA', 'RUS', 'UKR', 'POL', 'LTU'],
   BEL: ['NLD', 'DEU', 'LUX', 'FRA'],
   BLZ: ['MEX', 'GTM'],
   BEN: ['NER', 'NGA', 'TGO', 'BFA'],
-  BMU: [],
+  BMU: [], // Island
   BTN: ['CHN', 'IND'],
   BOL: ['BRA', 'PRY', 'ARG', 'CHL', 'PER'],
-  BES: [],
+  BES: [], // Island
   BIH: ['HRV', 'SRB', 'MNE'],
   BWA: ['NAM', 'ZWE', 'ZAF', ''],
-  BVT: [],
+  BVT: [], // Island
   BRA: ['VEN', 'GUY', 'SUR', 'GUF', 'URY', 'PRY', 'BOL', 'PER', 'COL'],
-  IOT: [],
+  IOT: [], // Island
   BRN: ['MYS'],
   BGR: ['ROU', 'TUR', 'GRC', 'MKD', 'SRB'],
   BFA: ['MLI', 'NER', 'BEN', 'TGO', 'GHA', 'CIV',],
   BDI: ['RWA', 'TZA', 'COD'],
-  CPV: [],
+  CPV: [], // Island
   KHM: ['THA', 'LAO', 'VNM'],
   CMR: ['NGA', 'TCD', 'CAF', 'COG', 'GAB', 'GNQ'],
   CAN: ['USA'],
-  CYM: [],
+  CYM: [], // Island
   CAF: ['TCD', 'SDN', 'SSD', 'COD', 'COG', 'CMR'],
-  TCD: [],
-  CHL: [],
-  CHN: [],
-  CXR: [],
-  CCK: [],
-  COL: [],
-  COM: [],
-  COD: [],
-  COG: [],
-  COK: [],
-  CRI: [],
-  CIV: [],
+  TCD: ['LBY', 'SSD', 'CAF', 'CMR', 'NGA', 'NER'],
+  CHL: ['PER', 'BOL', 'ARG'],
+  CHN: ['MNG', 'RUS', 'PRK', 'VNM', 'LAO', 'MMR', 'BTN', 'NPL', 'IND', 'TJK', 'KGZ', 'KAZ'],
+  CXR: [], // Island
+  CCK: [], // Island
+  COL: ['VEN', 'BRA', 'PER', 'ECU', 'PAN'],
+  COM: [], // Island
+  COD: ['CAF', 'SSD', 'UGA', 'RWA', 'TZA', 'ZMB', 'AGO', 'COG'],
+  COG: ['CAF', 'COD', 'AGO', 'GAB', 'CMR'],
+  COK: [], // Island
+  CRI: ['NIC', 'PAN'],
+  CIV: ['MLI', 'BFA', 'GHA', 'LBR', 'GIN'],
   HRV: ['SVN', 'HUN', 'SRB', 'BIH'],
-  CUB: [],
-  CUW: [],
-  CYP: [],
+  CUB: [], // Island
+  CUW: [], // Island
+  CYP: [], // Island
   CZE: ['POL', 'SVK', 'AUT', 'DEU'],
-  DNK: [],
-  DJI: [],
-  DMA: [],
-  DOM: [],
-  ECU: [],
-  EGY: [],
-  SLV: [],
-  GNQ: [],
-  ERI: [],
-  EST: [],
-  SWZ: [],
-  ETH: [],
-  FLK: [],
-  FRO: [],
-  FJI: [],
-  FIN: [],
-  FRA: [],
-  GUF: [],
-  PYF: [],
-  ATF: [],
-  GAB: [],
-  GMB: [],
-  GEO: [],
-  DEU: [],
-  GHA: [],
-  GIB: [],
-  GRC: [],
-  GRL: [],
-  GRD: [],
-  GLP: [],
-  GUM: [],
-  GTM: [],
-  GGY: [],
-  GIN: [],
-  GNB: [],
-  GUY: [],
-  HTI: [],
-  HMD: [],
-  VAT: [],
-  HND: [],
-  HKG: [],
+  DNK: ['NOR', 'DEU'],
+  DJI: ['ERI', 'SOM', 'ETH'],
+  DMA: [], // Island
+  DOM: ['HTI'],
+  ECU: ['COL', 'PER'],
+  EGY: ['ISR', 'SDN', 'LBY'],
+  SLV: ['GTM', 'HND'],
+  GNQ: ['CMR', 'GAB'],
+  ERI: ['SDN', 'DJI', 'ETH'],
+  EST: ['RUS', 'LVA'],
+  SWZ: ['ZAF', 'MOZ'],
+  ETH: ['ERI', 'DJI', 'SOM', 'KEN', 'SSD', 'SDN'],
+  FLK: [], // Island
+  FRO: [], // Island
+  FJI: [], // Island
+  FIN: ['NOR', 'RUS', 'SWE'],
+  FRA: ['BEL', 'LUX', 'CHE', 'ITA', 'ESP', 'AND'],
+  GUF: ['BRA', 'SUR'],
+  PYF: [], // Island
+  ATF: [], // Island
+  GAB: ['CMR', 'COG', 'GNQ'],
+  GMB: ['SEN'],
+  GEO: ['RUS', 'AZE', 'ARM', 'TUR'],
+  DEU: ['DNK', 'POL', 'CZE', 'AUT', 'CHE', 'FRA', 'LUX', 'BEL', 'NLD'],
+  GHA: ['BFA', 'TGO', 'CIV'],
+  GIB: ['ESP'],
+  GRC: ['MKD', 'BGR', 'TUR', 'ALB'],
+  GRL: [], // Island
+  GRD: [], // Island
+  GLP: [], // Island
+  GUM: [], // Island
+  GTM: ['MEX', 'BLZ', 'HND', 'SLV'],
+  GGY: [], // Island
+  GIN: ['MLI', 'CIV', 'LBR', 'SLE', 'GNB', 'GMB'],
+  GNB: ['GMB', 'GIN'],
+  GUY: ['SUR', 'BRA', 'VEN'],
+  HTI: ['DOM'],
+  HMD: [], // Island
+  VAT: ['ITA'],
+  HND: ['NIC', 'SLV', 'GTM'],
+  HKG: ['CHN'],
   HUN: ['SVK', 'UKR', 'ROU', 'SRB', 'HRV', 'SVN', 'AUT'],
-  ISL: [],
-  IND: [],
-  IDN: [],
-  IRN: [],
-  IRQ: [],
-  IRL: [],
-  IMN: [],
-  ISR: [],
+  ISL: [], // Island
+  IND: ['CHN', 'NPL', 'BTN', 'MMR', 'BGD', 'LKA', 'PAK', 'AFG'],
+  IDN: ['MYS', 'BRN', 'PNG'],
+  IRN: ['TKM', 'AFG', 'PAK', 'IRQ', 'TUR', 'AZE'],
+  IRQ: ['TUR', 'IRN', 'KWT', 'SAU', 'JOR', 'SYR'],
+  IRL: ['GBR'],
+  IMN: [], // Island
+  ISR: ['LBN', 'SYR', 'JOR', 'EGY'],
   ITA: ['AUT', 'SVN', 'FRA', 'CHE'],
-  JAM: [],
-  JPN: [],
-  JEY: [],
-  JOR: [],
-  KAZ: [],
-  KEN: [],
-  KIR: [],
+  JAM: [], // Island
+  JPN: [], // Island
+  JEY: [], // Island
+  JOR: ['SYR', 'IRQ', 'SAU', 'ISR'],
+  KAZ: ['RUS', 'CHN', 'KGZ', 'UZB', 'TKM'],
+  KEN: ['ETH', 'SOM', 'TZA', 'UGA', 'SSD'],
+  KIR: [], // Island
   XXK: ['SRB', 'MKD', 'ALB', 'MNE'],
   PRK: [],
   KOR: [],
